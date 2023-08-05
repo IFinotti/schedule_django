a673d1515a2f3643e211d428c4f7d9bdff252bc0
Isaac Finotti <imisaac2807@gmail.com>
2023-08-04 22:30:48 -0300

    Making some changes in the admin page.

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,38 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    # Shows these fields in the top of the DB
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'phone',
+    )
+
+    # Create a filter option
+    list_filter = ('created_date',)
+
+    # Sort contacts backwards based on id
+    ordering = ('-id',)
+
+    # A way to search the fields of any contact
+    search_fields = (
+        'id',
+        'first_name',
+        'last_name',
+    )
+
+    # Maximum of contacts in a single page
+    list_per_page = 10
+
+    # Limits the "Show all" option
+    list_max_show_all = 100
+
+    # Set these fields as editable at the index of DB
+    list_editable = (
+        'first_name',
+        'last_name',
+    )
+
+    # Clicking on these, it takes you to the info about the contact
+    list_display_links = ('id', 'phone')

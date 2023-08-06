362882efe9a6d9cbc57557f9d9bb7bc9755484a9
Isaac Finotti <imisaac2807@gmail.com>
2023-08-06 17:07:56 -0300

    Using a simple pagination.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 15)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        "contacts": contacts,
+        "page_obj": page_obj,
         "site_title": 'Contacts - '
     }
 
@@ -53,8 +58,12 @@ def search(request):
         )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 15)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        "contacts": contacts,
+        "page_obj": page_obj,
         "site_title": 'Search - ',
         "search_value": search_value,
     }

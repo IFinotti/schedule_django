c05c1b8c0bb14852feec6632f5befe73330e1e5f
Isaac Finotti <imisaac2807@gmail.com>
2023-08-06 15:36:21 -0300

    Creating the partials to use with 'include'.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,7 +8,10 @@ from django.http import Http404
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
 
-    context = {"contacts": contacts}
+    context = {
+        "contacts": contacts,
+        "site_title": 'Contacts - '
+    }
 
     return render(
         request,
@@ -21,7 +24,12 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
-    context = {"contact": single_contact}
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        "contact": single_contact,
+        "site_title": contact_name,
+    }
 
     return render(
         request,

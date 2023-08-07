b46f543d599902ba6583a2487cef2e94be4dbc90
Isaac Finotti <imisaac2807@gmail.com>
2023-08-07 20:12:37 -0300

    Validating the form fields with clean function.

--- contact/forms.py
+++ contact/forms.py
@@ -44,22 +44,33 @@ class ContactForm(forms.ModelForm):
         #     )
         # }
 
-    def clean(self):
-        # cleaned_data = self.cleaned_data
+    # Here you return the super and the clean. It can be used with more than one field at once
 
-        # self.add_error(
-        #     'first_name',
-        #     ValidationError(
-        #         'Mensagem de erro',
-        #         code='invalid'
-        #     )
-        # )
-        # self.add_error(
-        #     'first_name',
-        #     ValidationError(
-        #         'Mensagem de erro 2',
-        #         code='invalid'
-        #     )
-        # )
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first-name')
+        last_name = cleaned_data.get('first-name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'The first name cannot be identic as the last name.',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
         return super().clean()
+
+    # In the clean_(field) you've to return the name of the field
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Do not type "ABC"',
+                    code='invalid')
+            )
+
+        return first_name
